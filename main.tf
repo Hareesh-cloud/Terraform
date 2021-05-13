2d3ec1f5a99ebcb06392dd96fad6d4948a211d0b
Hareesh5041 <82581268+Hareesh5041@users.noreply.github.com>
2021-05-13 18:23:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Terraform configuration goes here
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.67.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-9999"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
